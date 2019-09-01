355afcb3824197f34f3d0fa278e09bde330fcf9e
Wodnyan <52217740+Wodnyan@users.noreply.github.com>
2019-09-01 17:37:29 +0300

    Redone the whole navbar

--- script/myscript.js
+++ script/myscript.js
@@ -2,18 +2,12 @@ const navSlide = () => {
     const burger = document.querySelector('.hamburger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
-    //Nav toggle
     burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        //Animated links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.8}s`;
-            }
-        });
+        nav.classList.toggle('nav-active')
     });
+    navLinks.forEach((link, index) => {
+
+    })
 }
 const app = () => {
     navSlide();
