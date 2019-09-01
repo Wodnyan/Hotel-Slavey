0d4fb90fb155818530bc9812cfa50e0d2cb6abdf
Wodnyan <52217740+Wodnyan@users.noreply.github.com>
2019-09-01 16:13:25 +0300

    Animated nav bar
    Animated nav bar and menu elements

--- script/myscript.js
+++ script/myscript.js
@@ -1,13 +1,23 @@
 const navSlide = () => {
     const burger = document.querySelector('.hamburger');
     const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelector('.nav-links li')
+    const navLinks = document.querySelectorAll('.nav-links li');
     //Nav toggle
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+        //Animated burger
+        burger.classList.toggle('toggle');
+        //Animated links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.8}s`;
+            }
+        });
     });
-    //Animated links
-    
 }
-
-navSlide();
+const app = () => {
+    navSlide();
+}
+app();
