2e064c9234524f27ea3976b8ba046ca3aa787c5a
Wodnyan <52217740+Wodnyan@users.noreply.github.com>
2019-09-14 13:04:02 +0300

    Navbar/burger fix

--- script/myscript.js
+++ script/myscript.js
@@ -1,15 +1,18 @@
 const navSlide = () => {
     const burger = document.querySelector('.hamburger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+    const nav = document.querySelector('nav');
+    const navLinks = document.querySelector('.nav-links');
+    const navLinksAll = document.querySelectorAll('.nav-links li');
     //Nav toggle
     burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        navLinks.forEach((link, index) => {
+        navLinks.classList.toggle('nav-active');
+        nav.classList.toggle('navBlack');
+        navLinksAll.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+
             }
         });
         burger.classList.toggle('active');
@@ -20,3 +23,9 @@ const app = () => {
     navSlide();
 };
 app();
+
+
+const date = new Date();
+
+
+console.log(date.toLocaleDateString());
